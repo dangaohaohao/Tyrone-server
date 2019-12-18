121dd09cfb31c3ab3c102ae84e0fb926be836de6
yuzhenliu <1109763686@qq.com>
2019-12-18 22:06:00 +0800

    12/18晚 写了新增订单和修改订单和订单首页的数据

--- model/Order.js
+++ model/Order.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const Order = mongoose.model('order', new mongoose.Schema({
   // 房屋编号
-  house: {
+  houseId: {
     type: mongoose.SchemaTypes.ObjectId,
     ref: 'House',
     required: true,
@@ -25,7 +25,7 @@ const Order = mongoose.model('order', new mongoose.Schema({
   },
   // 订单发起人 0:房东 / 1:房客 / 2:Agent / 3:平台
   orderAuthor: {
-    type: Number,
+    type: String,
     required: true,
   },
   // 发起人联系方式
@@ -35,12 +35,12 @@ const Order = mongoose.model('order', new mongoose.Schema({
   },
   // 订单类型 0:管理费 / 1:年度税金 / 2:生活代缴 / 3:维修服务 / 4:清扫服务 / 5:装修费
   orderType: {
-    type: Number,
+    type: String,
     required: true
   },
   // 具体内容 0:基础清洁 / 1:卫浴清洁 / 2:玻璃清洁 / 3:墙壁漏水 / 4:马桶维修 / 5:家电故障 / 6:门锁故障 / 7:代缴电费 / 8: 代缴水费 / 9:代缴物业费 / 10:代缴煤气费 / 11:其他
   content: {
-    type: Number,
+    type: String,
     default: 0,
   },
   // 预约时间
@@ -68,22 +68,21 @@ const Order = mongoose.model('order', new mongoose.Schema({
   },
   // 订单基础金额
   orderMoney: {
-    type: Number,
+    type: String,
     required: true,
   },
   // 订单 VAT 0:6% 1:9% 2:10% 3:15% 4:20% 5:25%
   orderVAT: {
-    type: Number,
+    type: String,
     required: true,
   },
   // 订单总金额
   orderSum: {
-    type: Number,
-    required: true
+    type: String,
   },
   // 支付方式 0:WeChat / 1:银行卡
   payWay: {
-    type: Number,
+    type: String,
   },
   // 银行账号
   bankAccount: {
@@ -109,4 +108,32 @@ module.exports.add = async (orderInfo) => {
   const order = new Order(orderInfo)
   return await order.save();
 }
+
+// 修改订单
+module.exports.modify = async (orderId, orderInfo) => {
+  let id = mongoose.Types.ObjectId(orderId);
+  return await Order.findByIdAndUpdate(id, orderInfo);
+}
+
+// 根据订单 id 来查找订单
+module.exports.findOrderById = async (orderId) => {
+  let id = mongoose.Types.ObjectId(orderId);
+  return await Order.findById({
+    "_id": id
+  });
+}
+
+// 根据房屋 id 搜索订单
+module.exports.findOrderByhouseId = async (houseId, skip, count) => {
+  let id = mongoose.Types.ObjectId(houseId);
+  return await Order.find({
+    houseId: id
+  }).skip(Number(skip)).limit(Number(count));
+}
+
+// 根据多个参数搜索订单
+module.exports.findOrderByMultiParams = async (orderType, payWay, orderStatus, skip, count) => {
+  return await Order.find({ orderType, payWay, orderStatus }).skip(Number(skip)).limit(Number(count));
+}
+
 
--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const Order = require('../model/Order');
+const { mock } = require("mockjs");
 const router = new express.Router();
 
 // 添加订单
 router.post('/add', (req,res)=>{
-  const {orderInfo} = req.body;
+  const orderInfo = req.body;
   Order.add(orderInfo)
-  .then((orderInfo)=>{
+  .then((orderInfor)=>{
     res.json({
       code: 0,
       message: 'ok',
@@ -21,6 +22,74 @@ router.post('/add', (req,res)=>{
     })
   })
 });
+
+// 修改房屋
+router.post('/modify', async (req, res) => {
+  let { orderId } = req.body;
+  let orderInfo = req.body;
+  Order.modify(orderId, orderInfo)
+  .then(orderInfor=>{
+    res.json({
+      code: 0, 
+      message: 'ok',
+      data: null
+    });
+  })
+  .catch(error=>{
+    res.json({
+      code: -1, 
+      message: error.message,
+      data: null
+    });
+  })
+});
+
+// 服务订单概述
+router.get('/getSummaryData', async (req, res) => {
+  res.json({
+    code: 0,
+    message: 'ok',
+    data: mock({
+      "addOrder|100-1000": 0,
+      "orderSum|10000-30000": 0,
+    })
+  });
+});
+
+// 根据订单编号搜索订单
+router.get('/findOrderById', async (req, res) => {
+  let {orderId} = req.query;
+  let orderInfo = await Order.findOrderById(orderId);
+  res.json({
+    code: 0,
+    message: 'ok',
+    data: orderInfo
+  })
+});
+
+// 根据房屋编号搜索订单
+router.get('/findOrderByhouseId', async (req, res) => {
+  let {houseId, skip, count} = req.query;
+  let orderInfo = await Order.findOrderByhouseId(houseId, skip, count);
+  res.json({
+    code: 0,
+    message: 'ok',
+    data: orderInfo
+  })
+});
+
+// 根据房屋编号搜索订单
+router.get('/findOrderByMultiParams', async (req, res) => {
+  let {orderType, payWay, orderStatus, skip, count} = req.query;
+  let orderInfo = await Order.findOrderByMultiParams(orderType, payWay, orderStatus, skip, count);
+  res.json({
+    code: 0,
+    message: 'ok',
+    data: orderInfo
+  })
+});
+
+
 
 
 module.exports = router;
