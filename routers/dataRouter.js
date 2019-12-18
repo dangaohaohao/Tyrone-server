226853e75ebd1691eb5eb3e03d6397dc83b7ddf6
yuzhenliu <1109763686@qq.com>
2019-12-18 20:28:42 +0800

    首页和数据管理的接口mock 好了

--- routers/dataRouter.js
+++ routers/dataRouter.js
@@ -6,10 +6,8 @@ const router = new express.Router();
 
 
 // mock 数据
- 
-// recharge:充值 / rent:房租缴纳 / service: 服务费用 / behelf:代缴费用
-// deposit:提现 / service: 服务费用支出 / behelf: 代缴费支出
-function mockRangeAccountData(begin, end){
+// mock 订单量变化趋势 
+function mockRangeOrderData(begin, end){
   let beginArr = begin.split('-');
   beginArr[1] = beginArr[1]-1;
   var a = moment(beginArr);
@@ -23,60 +21,125 @@ function mockRangeAccountData(begin, end){
   let date = new Date(begin);
   while(data.length <= result){
     data.push(mock({
-      date: moment(date).format('YYYY-MM-DD'),
-      in: {
-        "recharge|1000-10000": 0,
-        "rent|1000-30000": 0,
-        "service|1000-8000": 0,
-        "behelf|1000-5000": 0
-      },
-      out: {
-        "deposit|1000-10000": 0,
-        "service|1000-8000": 0,
-        "behelf|1000-5000": 0
-      }
+      date: moment(date).format('MM-DD'),
+      "orderData|10000-40000": 0,
     }));
     date.setDate(date.getDate()+1);
   };
   return data;
 }
-// mock 平台流水数据 
-router.get('/getAccountData', (req, res)=>{
+// mock 订单量变化趋势 
+router.get('/getOrderData', (req, res)=>{
   const {begin, end, count} = req.query;
+  if(count > 0){
+    res.json(mock({
+      code: 0,
+      message: 'ok',
+      ['data|'+count]: [
+        {
+          "orderData|10000-40000": 0,
+        }
+      ]})); 
+  }
+  else{
+    res.json({
+      code: 0,
+      message: 'ok',
+      data: mockRangeOrderData(begin, end)
+    });
+  }
+})
 
-  setTimeout(() => {
+// mock App下载量/Web UV趋势 
+function mockRangeDownLoadData(begin, end){
+  let beginArr = begin.split('-');
+  beginArr[1] = beginArr[1]-1;
+  var a = moment(beginArr);
 
+  let endArr = end.split('-');
+  endArr[1] = endArr[1]-1;
+  var b = moment(endArr);
+  var result = b.diff(a, 'days');
 
-    if(count > 0){
-      res.json(mock({
-        code: 0,
-        message: 'ok',
-        ['data|'+count]: [
-          {
-            in: {
-              "recharge|1000-10000": 0,
-              "rent|1000-30000": 0,
-              "service|1000-8000": 0,
-              "behelf|1000-5000": 0
-            },
-            out: {
-              "deposit|1000-10000": 0,
-              "service|1000-8000": 0,
-              "behelf|1000-5000": 0
-            }
-          }
-        ]})); 
-    }
-    else{
-      res.json({
-        code: 0,
-        message: 'ok',
-        data: mockRangeAccountData(begin, end)
-      });
-    }
-  }, 1000);
-  
-   
+  let data = [];
+  let date = new Date(begin);
+  while(data.length <= result){
+    data.push(mock({
+      date: moment(date).format('MM-DD'),
+      "appDownLoad|1000-4000": 0,
+      "WebUV|100-2000": 0,
+    }));
+    date.setDate(date.getDate()+1);
+  };
+  return data;
+}
+// mock App下载量/Web UV趋势 
+router.get('/getDownLoadData', (req, res)=>{
+  const {begin, end, count} = req.query;
+  if(count > 0){
+    res.json(mock({
+      code: 0,
+      message: 'ok',
+      ['data|'+count]: [
+        {
+          "appDownLoad|1000-4000": 0,
+          "WebUV|100-2000": 0,
+        }
+      ]})); 
+  }
+  else{
+    res.json({
+      code: 0,
+      message: 'ok',
+      data: mockRangeDownLoadData(begin, end)
+    });
+  }
+})
+
+// mock 订单日交易额数据情况
+function mockRangeTradingData(begin, end){
+  let beginArr = begin.split('-');
+  beginArr[1] = beginArr[1]-1;
+  var a = moment(beginArr);
+
+  let endArr = end.split('-');
+  endArr[1] = endArr[1]-1;
+  var b = moment(endArr);
+  var result = b.diff(a, 'days');
+
+  let data = [];
+  let date = new Date(begin);
+  while(data.length <= result){
+    data.push(mock({
+      date: moment(date).format('MM-DD'),
+      "orderData|100-1000": 0,
+      "tradingData|10000-80000": 0,
+    }));
+    date.setDate(date.getDate()+1);
+  };
+  return data;
+}
+// mock 订单日交易额数据情况 
+router.get('/getTradingData', (req, res)=>{
+  const {begin, end, count} = req.query;
+  if(count > 0){
+    res.json(mock({
+      code: 0,
+      message: 'ok',
+      ['data|'+count]: [
+        {
+          "orderData|100-1000": 0,
+          "tradingData|10000-80000": 0,
+        }
+      ]})); 
+  }
+  else{
+    res.json({
+      code: 0,
+      message: 'ok',
+      data: mockRangeTradingData(begin, end)
+    });
+  }
 })
 
 // mock 今日房屋动态
@@ -85,10 +148,25 @@ router.get('/getToadyData', (req, res) => {
     code: 0,
     message: 'ok',
     data: mock({
-      'addToday|200-1000': 1,
       'houseSum|2000-10000': 1,
+      'addToday|200-1000': 1,
       'rented|1000-5000': 1,
       'notLet|500-1000': 1,
+    })
+  });
+});
+
+// mock 应用商城下载分布
+router.get('/getApplicationData', (req, res) => {
+  res.json({
+    code: 0,
+    message: 'ok',
+    data: mock({
+      'appStore|20000-40000': 1,
+      'pea|20000-40000': 1,
+      '360|20000-40000': 1,
+      'application|20000-40000': 1,
+      'android|20000-40000': 1,
     })
   });
 });
